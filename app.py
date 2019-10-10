69d6664dca57d7a5df3aadb38433f3a692cd0888
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-10-09 23:55:27 -0700

    completed product page

--- app.py
+++ app.py
@@ -18,32 +18,33 @@ db_products = [
         'name': '1',
         'description': 'desc1',
         'category': 'cat1',
-        'picture': './static/placeholder-img.png',
+        'picture': '/static/placeholder-img.png',
         'price': 30.00
     },
     {
         'name': '2',
         'description': 'desc2',
         'category': 'cat2',
-        'picture': './static/placeholder-img.png',
+        'picture': '/static/placeholder-img.png',
         'price': 25.00
     },
     {
         'name': '3',
         'description': 'desc3',
         'category': 'cat3',
-        'picture': './static/placeholder-img.png',
+        'picture': '/static/placeholder-img.png',
         'price': 40.00
     },
     {
         'name': '4',
         'description': 'desc4',
         'category': 'cat4',
-        'picture': './static/placeholder-img.png',
+        'picture': '/static/placeholder-img.png',
         'price': 15.00
     }
 ]
 
+# db.products.delete_many({})
 # db.products.insert_many(db_products)
 
 app = Flask(__name__)
@@ -54,7 +55,16 @@ def index():
 
 @app.route('/product/<product_id>', methods=['GET'])
 def product_page(product_id):
-    return "i work"
+    product = products.find_one({'_id': ObjectId(product_id)})
+    return render_template('product.html', product=product)
+
+@app.route('/product/<product_id>', methods=['POST'])
+def add_product(product_id):
+    return redirect(url_for('show_shopping_cart'))
+
+@app.route('/shopping-cart', methods=["GET"])
+def show_shopping_cart():
+    return render_template('shopping-cart.html', msg="hi")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
